cf3990dc64ca0772f98f4e5fb6a3f3fcd17ad4b4
DevN <msnayal.55@gmail.com>
2025-02-20 18:48:14 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Security, LoginCallback } from '@okta/okta-react';
-import { AuthProvider } from './context/AuthContext';
-import Chatbox from './components/Chatbox';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const OKTA_CONFIG = {
-  clientId: 'your-client-id',
-  issuer: 'https://your-okta-domain.com/oauth2/default',
-  redirectUri: window.location.origin + '/login/callback',
-  scopes: ['openid', 'profile', 'email'],
-};
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-function App() {
-  return (
-    <AuthProvider>
-      <Security {...OKTA_CONFIG}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Chatbox />} />
-            <Route path="/login/callback" element={<LoginCallback />} />
-          </Routes>
-        </Router>
-      </Security>
-    </AuthProvider>
-  );
-}
-
-export default App;
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
